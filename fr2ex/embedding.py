937be55bd2f068e0fbc9d32a3a7ddc77ba88a859
Eliah Kagan <degeneracypressure@gmail.com>
2022-12-28 06:16:59 -0500

    More specific type annotations for embed_many

--- fr2ex/embedding.py
+++ fr2ex/embedding.py
@@ -5,20 +5,25 @@ The text embeddings computed are computed with OpenAI's new (in December 2022)
 model text-embedding-ada-002.
 """
 
-__all__ = ['embed_many']
+__all__ = ['EmbeddingsMatrix', 'embed_many']
 
+from nptyping import Float32, NDArray, Shape, assert_isinstance
 import numpy as np
-from numpy.typing import NDArray
 import openai.embeddings_utils
 
 from fr2ex import _task
 
+EmbeddingsMatrix = NDArray[Shape['*, 1536'], Float32]
+"""A matrix whose rows are embeddings in a 1536-dimensional space."""
+
 
 @_task.api_task('embeddings')
-def embed_many(texts: list[str]) -> NDArray[np.float32]:
+def embed_many(texts: list[str]) -> EmbeddingsMatrix:
     """Load or query the API for text-embedding-ada-002 for all texts."""
     embeddings = openai.embeddings_utils.get_embeddings(
         list_of_text=texts,
         engine='text-embedding-ada-002',
     )
-    return np.array(embeddings, np.float32)
+    matrix = np.array(embeddings, Float32)
+    assert_isinstance(matrix, EmbeddingsMatrix)
+    return matrix
